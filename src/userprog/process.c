1fea1defecfc19740d80d27c6ca52f4286a0cf5c
seong beom <c20944@kma.ac.kr>
2025-12-08 02:30:07 +0900

    ddddd

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -157,11 +157,11 @@ void
 process_exit (void)
 {
   struct thread *cur = thread_current ();
-  uint32_t *pd;
+  uint32_t *pd cur->pagedir;
 
   /* Destroy the current process's page directory and switch back
      to the kernel-only page directory. */
-  pd = cur->pagedir;
+
   if (pd != NULL) 
     {
       /* Correct ordering here is crucial.  We must set
