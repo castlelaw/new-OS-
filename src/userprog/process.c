91ca84c6fe4041d0e2aecdbf872b96889e0cf295
seong beom <c20944@kma.ac.kr>
2025-12-08 00:06:43 +0900

    ddd

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -380,7 +380,7 @@ load (const char *file_name, void (**eip) (void), void **esp)
   file_close (file);
   return success;
 }
-
+
 /* load() helpers. */
 
 static bool install_page (void *upage, void *kpage, bool writable);
