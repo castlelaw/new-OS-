da6218fd64326b4aa829aeb4bcda0bc8cc0f60b7
minkyu04 <c21110@kma.ac.kr>
2025-12-27 18:21:23 +0900

    Update thread.h

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -24,12 +24,19 @@ typedef int tid_t;
 #define PRI_DEFAULT 31
 #define PRI_MAX 63
 
-/* [P2-1 FIX] 자식 프로세스 정보를 담는 구조체 */
+/* [P2-1 FIX] 자식 프로세스 정보를 담는 구조체
+   - 부모 thread의 children 리스트에 매달려 있음
+   - wait()는 "자식 1개당 딱 1번"만 가능해야 하므로 waited 플래그 필요
+   - 자식이 이미 종료했는지 여부(exited)를 기록해두면 process_wait 구현이 더 안전해짐 */
 struct child_process {
-    tid_t tid;                 /* 자식 스레드 ID */
-    int exit_status;           /* 자식 종료 코드 */
-    struct semaphore wait_sema;/* 자식 종료 대기용 세마포어 */
-    struct list_elem elem;     /* 부모의 children 리스트 연결용 */
+    tid_t tid;                  /* 자식 스레드 ID */
+    int exit_status;            /* 자식 종료 코드 (exit(status) 또는 -1) */
+
+    bool waited;                /* 부모가 이미 wait() 했는지 (1회 제한용) */
+    bool exited;                /* 자식이 종료했는지 여부 (자식 exit 시 true) */
+
+    struct semaphore wait_sema; /* 자식 종료 대기용 세마포어 (자식 exit에서 up) */
+    struct list_elem elem;      /* 부모의 children 리스트 연결용 */
 };
 
 struct thread
@@ -54,6 +61,7 @@ struct thread
     /* [P2-1 FIX] 자식 관리 필드 */
     struct list children;      /* 자식 프로세스 목록 (struct child_process) */
     struct child_process *cp;  /* 나 자신의 메타데이터 포인터 (부모가 만들어준 것) */
+    struct file *fd_table[128]; /* syscall.c에서 2~127을 사용 *
 #endif
 
     unsigned magic;
